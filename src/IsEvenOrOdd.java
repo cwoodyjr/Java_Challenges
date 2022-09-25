4a245e968ba982cc0a9b284e416632b65be6c7c3
Craig <craig.woodcock89@hotmail.co.uk>
2022-09-25 12:22:53 +0100

    added if statement to IsEvenOrOdd within the main method to show same outcome

--- src/IsEvenOrOdd.java
+++ src/IsEvenOrOdd.java
@@ -4,10 +4,10 @@ public class IsEvenOrOdd {
 
     public static boolean isEven(int n) {// boolean here returns true or false value.
         return n%2==0; // int n will use the first integer declared in main class.
-        // here int n = userNum.
-        // % is modulo (finds remainder), here %2 finds remainder when input is
-        // divided by 2. if == 0, number is even.
-        // if result ==0, returns true. if result =>1, returns false
+                        // here int n = userNum.
+                        // % is modulo (finds remainder), here %2 finds remainder when input is
+                        // divided by 2. if == 0, number is even.
+                        // if result ==0, returns true. if result =>1, returns false
     }
 
     public static void main(String[] args) {
@@ -15,6 +15,12 @@ public class IsEvenOrOdd {
 
         System.out.println("Enter a number: ");
         int userNum = scanner.nextInt();
-        System.out.println("Is the number even? " + isEven(userNum));
+        System.out.println("Is the number even? " +isEven(userNum));    // this line would be changed to +userNum when using the if statement
+//        if (userNum%2==0){                            // here we achieve the same outcome using an if statement
+                                                        // inside the main method.
+//            System.out.println("that's true");
+//        }else {
+//            System.out.println("false");
+//        }
     }
 }
