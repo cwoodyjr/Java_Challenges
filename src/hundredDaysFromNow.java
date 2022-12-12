b0865aa9ce9c5ae0af1017a9c8b48a8eaa6d8886
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-12 10:55:27 +0000

    created 100 days from now

---
+++ src/hundredDaysFromNow.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+
+public class hundredDaysFromNow {
+
+    static LocalDate calculateHundredDaysFromNow(LocalDate today){
+        Period hundredDays = Period.ofDays(100);
+        return today.plus(hundredDays);
+    }
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now(ZoneId.of("Europe/London"));
+        System.out.println("100 days from now is "+calculateHundredDaysFromNow(today));
+
+    }
+}
